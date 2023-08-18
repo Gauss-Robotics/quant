64f3cfa7da1f156072e40759be79989640a78343
Christian Dreher <c.dreher@kit.edu>
2023-08-18 21:36:58 +0200

    feat: Convenience CMake util to find dependencies of quant when finding quant

---
+++ cmake/quantConfig.template.cmake
@@ -0,0 +1,10 @@
+cmake_minimum_required(VERSION 3.10)
+
+# Add custom Find*.cmake files to CMAKE_MODULE_PATH.
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "@CMAKE_SOURCE_DIR@/cmake/")
+
+# Perform a find_package for dependencies of quant.
+find_package(Eigen3 REQUIRED)
+
+# Include auto-generated cmake Config file.
+include("@PROJECT_BINARY_DIR@/quantConfig-autogen.cmake")
