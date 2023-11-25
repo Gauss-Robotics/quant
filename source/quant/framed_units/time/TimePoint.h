187211dfb78e7e16d036560259ec81aa8536aedf
Christian Dreher <c.dreher@kit.edu>
2023-11-25 11:35:30 +0100

    feat: Implement to_microseconds for framed time point

--- source/quant/framed_units/time/TimePoint.h
+++ source/quant/framed_units/time/TimePoint.h
@@ -2,6 +2,7 @@
 
 #include <quant/framed_geometry/Framed.h>
 #include <quant/framed_units/time/forward_declarations.h>
+#include <quant/units/Scalar.h>
 #include <quant/units/time/TimePoint.h>
 
 namespace quant::framed_units::time
@@ -11,10 +12,22 @@ namespace quant::framed_units::time
     {
 
     public:
+        using Framed<units::time::TimePoint>::Framed;
+
         TimePoint(units::time::TimePoint const& t, FrameData const& frame) :
             Framed<units::time::TimePoint>(t, frame)
         {
             ;
+        }
+
+        /**
+         * @brief Returns the amount of microseconds.
+         * @return Amount of microseconds.
+         */
+        units::Scalar
+        to_microseconds() const
+        {
+            return get_framed_object().to_microseconds();
         }
     };
 
