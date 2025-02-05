2658d4180cab984bc7f37714dd47fb6281461de5
Christoph Pohl <13176160+chpohl@users.noreply.github.com>
2025-02-05 09:40:42 +0100

    Improve simple robot example

--- examples/simple_robot/SimpleRobot.h
+++ examples/simple_robot/SimpleRobot.h
@@ -11,36 +11,46 @@ namespace simple_robot_example
     class SimpleRobot
     {
     public:
-        SimpleRobot(quant:: FramedPose const& origin)
+        SimpleRobot(quant::FramedPose const& origin)
         {
             using namespace quant;
 
-             FramedPose const root = origin.enframe(
-                Pose(Position::millimeters({.x = 300}),
-                                         Orientation::radians(AxisAngle::around_z(2))),
-                frames.root);
+            FramedPose const root =
+                origin.enframe(Pose(Position::millimeters({.x = 300}),
+                                    Orientation::radians(AxisAngle::around_z(2))),
+                               frames.root);
 
             this->root = root;
 
-             FramedPose const chest =
-                root.enframe(Pose(), frames.chest);
+            FramedPose const chest =
+                root.enframe(Pose(Position::millimeters({10, 0, 10}),
+                                  Orientation::degrees(AxisAngle::around_x(90))),
+                             frames.chest);
 
-             FramedPose const arm_right =
-                chest.enframe(Pose(), frames.arm_right);
+            FramedPose const arm_right =
+                chest.enframe(Pose(Position::millimeters({0, 0, 100})), frames.arm_right);
 
-             FramedPose const tcp_right =
-                arm_right.enframe(Pose(), frames.tcp_right);
+            FramedPose const tcp_right = arm_right.enframe(
+                Pose(Orientation::degrees({.axis = {0, 1, 0}, .angle = 45})), frames.tcp_right);
 
-             FramedPose const arm_left =
-                chest.enframe(Pose(), frames.arm_left);
+            FramedPose const arm_left =
+                chest.enframe(arm_right.get_framed_object().inverse(), frames.arm_left);
 
-             FramedPose const tcp_left =
-                arm_left.enframe(Pose(), frames.tcp_left);
+            FramedPose const tcp_left =
+                arm_left.enframe(Pose(-arm_right.get_framed_object().linear(),
+                                      arm_right.get_framed_object().angular()),
+                                 frames.tcp_left);
 
-             FramedPose const camera =
-                chest.enframe(Pose(), frames.camera);
+            FramedPose const camera = chest.enframe(Pose(Position::millimeters({100, 100, 100})), frames.camera);
 
             _frames[std::string(root.get_name())] = root;
+            _frames[std::string(chest.get_name())] = chest;
+            _frames[std::string(arm_right.get_name())] = arm_right;
+            _frames[std::string(tcp_right.get_name())] = tcp_right;
+            _frames[std::string(arm_left.get_name())] = arm_left;
+            _frames[std::string(tcp_left.get_name())] = tcp_left;
+            _frames[std::string(camera.get_name())] = camera;
+            _frames[std::string(origin.get_name())] = origin;
         }
 
         void
@@ -52,7 +62,12 @@ namespace simple_robot_example
         quant::FramedPose
         detect_object() const
         {
-            quant::Pose const object_pose;
+
+            quant::Pose const object_pose(
+                quant::Position::millimeters(
+                    quant::units::Vector::from_eigen(Eigen::Vector3d::Random())),
+                quant::Orientation::radians(
+                    quant::AxisAngle::from_eigen(Eigen::Quaterniond::UnitRandom())));
             return quant::FramedPose(object_pose, {.name = "Cup", .base_frame = frames.camera});
         }
 
@@ -73,12 +88,62 @@ namespace simple_robot_example
             std::string camera = "Robot::Camera";
         } const frames;
 
-        quant:: FramedPose root = quant:: FramedPose(
-            quant::Pose(quant::Position::millimeters({.x = 42})),
-             {.name="asdf"});
+        quant::BaseChange get_frame_transformation(std::string const& from_frame,
+                                                   std::string const& to_frame) const
+        {
+            const auto from_pose = _frames.at(from_frame);
+            const auto to_pose = _frames.at(to_frame);
+            if (from_pose.get_base_frame() != to_pose.get_base_frame())
+            {
+                try {
+                    quant::FramedPose from_pose_in_root;
+                    quant::FramedPose to_pose_in_root;
+                    if (from_pose.get_base_frame() != frames.root)
+                    {
+                        from_pose_in_root = transform_to_frame(from_pose, frames.root);
+                    }
+                    else
+                    {
+                        from_pose_in_root = from_pose;
+                    }
+                    if (to_pose.get_name() == frames.root)
+                    {
+                        return quant::BaseChange{.from_frame = from_frame,
+                                                 .to_frame = to_frame,
+                                                 .transformation = quant::SpatialDisplacement(from_pose_in_root.get_framed_object())};
+                    }
+                    if (to_pose.get_base_frame() != frames.root)
+                    {
+                        to_pose_in_root = transform_to_frame(to_pose, frames.root);
+                    }
+                    else
+                    {
+                        to_pose_in_root = to_pose;
+                    }
+                    return quant::BaseChange{.from_frame = from_frame,
+                                             .to_frame = to_frame,
+                                             .transformation = (to_pose_in_root - from_pose_in_root).get_framed_object()};
+                } catch (std::out_of_range const&) {
+                    throw std::runtime_error("Frames are not connected.");
+                }
+            }
+            return quant::BaseChange{.from_frame = from_frame,
+                                     .to_frame = to_frame,
+                                     .transformation = (to_pose - from_pose).get_framed_object()};
+        }
+
+        quant::FramedPose transform_to_frame(const quant::FramedPose& pose, std::string const& to_frame) const
+        {
+            const auto base_change = get_frame_transformation(std::string(pose.get_base_frame()), to_frame);
+            return base_change * pose;
+        }
+
+        quant::FramedPose root =
+            quant::FramedPose(quant::Pose(quant::Position::millimeters({.x = 42})),
+                              {.name = frames.root});
 
     private:
-        std::map<std::string, quant:: FramedPose> _frames;
+        std::map<std::string, quant::FramedPose> _frames;
     };
 
 }  // namespace simple_robot_example
